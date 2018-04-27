fdeec35542c2ec73b17de72cd5d3a09a9b761c53
Nimmly <marko.v.popovic@gmail.comgot config --global user.name Nimmlygit config --global user.email marko.v.popovic@gmail.com>
2018-04-27 18:44:39 +0200

    first

---
+++ src/components/NavBar.vue
@@ -0,0 +1,21 @@
+<template>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item">
+        <router-link class="nav-link " to="customers">Customers</router-link>
+      </li>
+      <li class="nav-item">
+      <router-link class="nav-link " to="products">Products</router-link>
+      </li> 
+    </ul>
+  </div>
+</nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
